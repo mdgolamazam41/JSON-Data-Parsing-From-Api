1596f62295c5f27f695649bf37f619197e55b61e
silentlearnerGA <mg.azam41@gmail.com>
2016-10-22 16:13:20 +0600

    jSON webrequest api testing

---
+++ webrequest-test/SWPerson.swift
@@ -0,0 +1,41 @@
+//
+//  SWPerson.swift
+//  webrequest-test
+//
+//  Created by Mark Price on 8/14/15.
+//  Copyright © 2015 devslopes. All rights reserved.
+//
+
+import Foundation
+
+class SWPerson {
+    private var _name: String!
+    private var _height: String!
+    private var _birthYear: String!
+    private var _hairColor: String!
+    
+    var name: String {
+        return _name
+    }
+    
+    var height: String {
+        get {
+            return _height
+        }
+    }
+    
+    var birthYear: String {
+        return _birthYear
+    }
+    
+    var hairColor: String {
+        return _hairColor
+    }
+    
+    init(name: String, height: String, birthYear: String, hairColor: String) {
+        _name = name
+        _height = height
+        _birthYear = birthYear
+        _hairColor = hairColor
+    }
+}
--- webrequest-test/ViewController.swift
+++ webrequest-test/ViewController.swift
@@ -12,14 +12,45 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let urlString = "http://swapi.co/api/people/1/"
+        let session = NSURLSession.sharedSession()
+        let url = NSURL(string: urlString)!
+        
+        session.dataTaskWithURL(url) { (data: NSData?, response:NSURLResponse?, error: NSError?) -> Void in
+            
+            if let responseData = data {
+                
+                do {
+                    let json = try NSJSONSerialization.JSONObjectWithData(responseData, options: NSJSONReadingOptions.AllowFragments)
+                    
+                    if let dict = json as? Dictionary<String, AnyObject> {
+                        
+                        if let name = dict["name"] as? String, let height = dict["height"] as? String, let birth = dict["birth_year"] as? String, let hair = dict["hair_color"] as? String {
+                            
+                            let person = SWPerson(name: name, height: height, birthYear: birth, hairColor: hair)
+                            
+                            print(person.name)
+                            print(person.height)
+                            print(person.hairColor)
+                            print(person.birthYear)
+                            
+                            if let films = dict["films"] as? [String] {
+                                for film in films {
+                                    print(film)
+                                }
+                            }
+                            
+                        }
+                    }
+                    
+                    
+                } catch {
+                    print("Could not serialize")
+                }
+            }
+        }.resume()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
